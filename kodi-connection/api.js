6453dfb342fd67b4dfa4fd6685312df6bfc7262c
OmerTu <omertu@gmail.com>
2017-12-16 12:56:56 -0500

    Add files via upload

--- kodi-connection/api.js
+++ kodi-connection/api.js
@@ -1,57 +1,69 @@
-module.exports = function(fetch) {
-  var namespaces = require('./api-methods.js');
-  var kodi_auth;
+const namespaces = require('./api-methods.js');
+const ResponseException = require('../exceptions.js').ResponseException;
 
-  function addMethods(obj) {
-    namespaces.forEach(function(namespace) {
-      obj[namespace.name] = namespace.methods.reduce(function(result, method) {
-        result[method] = function(params, callback) {
-          return obj.send(namespace.name + '.' + method, params, callback);
+module.exports = (fetch) => {
+    
+    const addMethods = (obj) => {
+        namespaces.forEach((namespace) => {
+            obj[namespace.name] = namespace.methods.reduce((result, method) => {
+                result[method] = (params, callback) => {
+                    return obj.send(`${namespace.name}.${method}`, params, callback);
+                };
+                return result;
+            }, {});
+        });
+    };
+
+    const Kodi = function(protocol, ip, port, username, password) {
+        this.kodiAuth = `Basic ${new Buffer(`${username}:${password}`).toString('base64')}`;
+        this.url = `${protocol}://${ip}:${port}/jsonrpc`;
+        addMethods(this);
+    };
+
+    Kodi.prototype.sendHTTP = function(body, callback) {
+        console.log(`Command sent = ${body}`);
+        const headers = {
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': this.kodiAuth
         };
-        return result;
-      }, {});
-    });
-  }
 
-  function Kodi(ip, port, username, password) {
-    kodi_auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
-    this.url = 'http://' + ip + ':' + port + '/jsonrpc';
-    addMethods(this);
-  }
+        return fetch(this.url, {
+            method: 'POST',
+            body: body,
+            headers: headers
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
 
-  Kodi.prototype.sendHTTP = function(body, callback) {
-    console.log('Command sent = ' + body);
-    var headers = {
-      "Content-type": "application/json",
-      'Accept': "application/json",
-      'Authorization': kodi_auth
+            throw new ResponseException(
+                `Error in response, ${response.statusText} with status code: ${response.status}`,
+                response.status,
+                response.statusText);
+        })
+        .then((data) => {
+            if (callback) {
+                callback(data);
+            }
+            return data;
+        });
     };
 
-    return fetch(this.url, {
-        method: 'POST',
-        body: body,
-        headers: headers
-      })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function(data) {
-        if(callback) callback(data);
-        return data;
-      });
-  };
+    Kodi.prototype.send = function(method, params, callback) {
+        let body = {
+            jsonrpc: '2.0',
+            id: 1,
+            method: method
+        };
 
-  Kodi.prototype.send = function(method, params, callback) {
-    var body = {
-      jsonrpc: "2.0",
-      id: 1,
-      method: method
+        if (params) {
+            body.params = params;
+        }
+        body = JSON.stringify(body);
+        return this.sendHTTP(body, callback);
     };
 
-    if(params) body.params = params;
-    body = JSON.stringify(body);
-    return this.sendHTTP(body, callback);
-  };
-
-  return Kodi;
+    return Kodi;
 };
